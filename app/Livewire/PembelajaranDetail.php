0d254dfc2f227b55a1124a8e3e523d35611f341b
Albert Gultom <gultomic@gmail.com>
2024-06-07 09:42:56 +0700

    funcgsi pembelajaran

--- app/Livewire/PembelajaranDetail.php
+++ app/Livewire/PembelajaranDetail.php
@@ -8,17 +8,30 @@ use App\Models\Pembelajaran as PB;
 
 class PembelajaranDetail extends Component
 {
-    #[Layout('layouts.app')]
-
     public $pmb;
+    public $mcid;
 
     public function mount($pbid)
     {
         $this->pmb = PB::with('materi')->find($pbid);
+        $this->mcid = $this->pmb->microlearning_id;
     }
 
+    #[Layout('layouts.app')]
     public function render()
     {
         return view('livewire.pembelajaran-detail');
+    }
+
+    public function destroy()
+    {
+        try {
+            $this->pmb->delete();
+            session()->flash('success', 'Berhasil menghapus pembelajaran!');
+        } catch (Exception $e) {
+            session()->flash('error', $e->getMessage());
+        }
+
+        $this->redirectRoute('admin.micl.detail', ['mcid' => $this->mcid]);
     }
 }
